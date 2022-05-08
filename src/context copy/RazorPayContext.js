fd03afedfd657dbe73bc70e0665153a0838b03d7
Two Man <twoman44@gmail.com>
2022-05-08 20:54:19 +0530

    backup commit

---
+++ src/context copy/RazorPayContext.js
@@ -0,0 +1,22 @@
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_Y5tlqQ0O8yOPLG",
+  key_secret: "jT3RnPFiYSDkwqg2i6Tdpu1V",
+});
+
+export const createOrder = () => {
+  const options = {
+    amount: 199900,
+    currency: "INR",
+    receipt: "order#1",
+  };
+  razorpay.orders.create(options, function (err, order) {
+    if (order) {
+      console.log(order);
+      return order.id;
+    } else {
+      console.log(err);
+    }
+  });
+};
